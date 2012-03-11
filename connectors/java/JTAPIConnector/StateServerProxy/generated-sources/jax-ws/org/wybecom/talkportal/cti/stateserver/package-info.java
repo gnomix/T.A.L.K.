5afc744a525d9bdccb30c9cc4f30143b927c3b00
Hervé Boutemy <herve.boutemy@free.fr>
2012-03-11 21:38:24 +0100

    created a parent pom with a multi-modules Maven build

--- connectors/java/JTAPIConnector/StateServerProxy/generated-sources/jax-ws/org/wybecom/talkportal/cti/stateserver/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://wybecom.org/talkportal/cti/stateserver", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.wybecom.talkportal.cti.stateserver;
